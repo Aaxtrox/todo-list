b0003b0d4d4b36a90a746268525d8bde9c84ce68
Pawel Wojciechowski <pawelw@gmx.com>
2024-01-16 10:44:23 +0000

    Add 'today' function

---
+++ src/today.js
@@ -0,0 +1,8 @@
+// Define a function named 'today' using function expression syntax
+const today = function () {
+    // Log the string 'TODAY' to the console
+    console.log('TODAY');
+};
+
+// Export the 'today' function as the default export of this module
+export default today;
