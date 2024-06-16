5221e94d46f659b36b3283df585ae330a286b310
Pawel Wojciechowski <pawelw@gmx.com>
2024-06-16 17:53:00 +0200

    Remove unnecessary files

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@
 import './style.css';
 
 // Import functions from other files
-import localStorage from './localStorage';
 import menu from './menu';
 import input from './input';
 
 // Call functions
-localStorage;
 menu();
 input();
