086ce5a5161e003e0d6196fbf8b3a5641feca3cc
Pawel Wojciechowski <pawelw@gmx.com>
2024-06-16 16:58:51 +0200

    refactor: update returnMenu to display menu immediately

--- src/returnMenu.js
+++ src/returnMenu.js
@@ -4,15 +4,9 @@ import input from './input';
 
 // Create a function called 'returnMenu'
 const returnMenu = function () {
-    // Use the 'prompt' function to get user input and store it in 'userInput'
-    let userInput = prompt('Enter a number 1 to return to the menu');
-
-    // If the user's input is a number 1
-    if (userInput === '1') {
-        // Call functions
-        menu();
-        input();
-    }
+    // Call the 'menu' function
+    menu();
+    input();
 };
 
 // Export the 'returnMenu' function as the default export of this module
