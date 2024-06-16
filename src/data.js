1eee375ea68efca5bbba5ff14f8e6059ea88d948
Pawel Wojciechowski <pawelw@gmx.com>
2024-06-16 12:41:58 +0200

    Modify temp data

--- src/data.js
+++ src/data.js
@@ -4,9 +4,9 @@ const data = function () {
     let lists = [];
 
      // Push objects representing lists with tasks and due dates into the 'lists' array
-    lists.push({name: 'list1', tasks: [{name: 'task1', dueDate: '2024-01-17'}, {name: 'task2', dueDate: '2024-01-18'}]});
-    lists.push({name: 'list2', tasks: [{name: 'task3', dueDate: '2024-01-17'}, {name: 'task4', dueDate: '2024-01-19'}]});
-    lists.push({name: 'list3', tasks: [{name: 'task5', dueDate: '2024-01-25'}, {name: 'task6', dueDate: '2024-03-17'}]});
+    lists.push({name: 'list1', tasks: [{name: 'task1', dueDate: '2024-06-17'}, {name: 'task2', dueDate: '2024-06-18'}]});
+    lists.push({name: 'list2', tasks: [{name: 'task3', dueDate: '2024-06-17'}, {name: 'task4', dueDate: '2024-06-19'}]});
+    lists.push({name: 'list3', tasks: [{name: 'task5', dueDate: '2024-06-25'}, {name: 'task6', dueDate: '2024-09-17'}]});
 
     // Return the 'lists' array
     return lists;
